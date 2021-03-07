9fd8690e02a8142b1d45ed9eeef591ac8b232db1
Konrad Bartlett <Konrad.Bartlett@Outlook.com>
2021-03-07 14:24:24 -0500

    updated script tag

--- konrad-secret-button/script-tags/tag.js
+++ konrad-secret-button/script-tags/tag.js
@@ -1,3 +1,8 @@
-import './style.css'
-
 console.log('i am here!')
+
+var secretButton = document.querySelector('.secret');
+console.log(secretButton);
+secretButton.addEventListener('click', function () {
+    console.log('open modal: ', this.dataset.product);
+    alert('woot woot')
+});
