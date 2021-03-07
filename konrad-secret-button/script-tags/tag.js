ff2000672ecf71094ffd0e00db94671ea61e3655
Konrad Bartlett <Konrad.Bartlett@Outlook.com>
2021-03-07 16:50:10 -0500

    updated tag logic

--- konrad-secret-button/script-tags/tag.js
+++ konrad-secret-button/script-tags/tag.js
@@ -1,13 +1,34 @@
-import { global } from "../utils/global";
-import { SecretModal } from '../components/secretModal';
-
 console.log('i am here!')
 
 var secretButton = document.querySelector('.secret');
-console.log(secretButton);
 secretButton.addEventListener('click', function () {
     console.log('open modal: ', this.dataset.product);
-    alert('woot woot')
-    console.log(global());
-    document.getElementById('content').appendChild(SecretModal)
+    alert('woot woot');
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    };
+    let guesses = getCookie('guesses');
+    let guess = prompt(`You have ${guesses} guesses remaining`)
+    return fetch(`${process.env.HOST}/guess?guess=${guess}`, requestOptions).then(
+        result => {
+            console.log(result);
+            if (result.result == "low") {
+                alert('That guess was too low');
+            } else if (result.result == "high") {
+                alert('That guess was too high');
+            } else if (result.result == "correct") {
+                window.location = "http://www.google.com";
+            }
+        }
+    );
+
 });
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
